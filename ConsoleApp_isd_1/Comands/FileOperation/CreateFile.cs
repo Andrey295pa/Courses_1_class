6eadcd4a9d69b1d102e2c944190077c56b707657
Andrey <avpahomov295@gmail.com>
2019-02-28 21:27:56 +0200

    second commit

---
+++ ConsoleApp_isd_1/Comands/FileOperation/CreateFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_isd_1.Comands.FileOperation
+{
+    class CreateFile : IComand
+    {
+        public void execute(string[] arg)
+        {
+            File.Create(arg[1] + "\\Mytest.txt");
+        }
+    }
+}
---
+++ ConsoleApp_isd_1/Comands/FileOperation/DeleteFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_isd_1.Comands.FileOperation
+{
+    class DeleteFile : IComand
+    {
+        public void execute(string[] arg)
+        {
+            File.Delete(arg[1] + "\\Mytest.txt");
+        }
+    }
+}
